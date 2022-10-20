0522d207a93b0909eb23e67cefa5cbc2e1a12829
AlexanderDjHansen <alexander.hansen@live.com>
2022-10-20 12:10:05 +0200

    Sista kommentar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Counter from "./components/Counter";
 import TemperaturOmvandlare from "./components/TemperaturOmvandlare";
 import Timer from "./components/Timer";
 
+// Det här med att skapa en ram var oklart för mig. Hoppas det ser okej ut trots det.
+
 function App() {
   return (
     <div className="App">
